70f54afb83b118e1f6bb4647824c1e50fd9fbbd1
Machine Learning & Bioinformatics Lab <82195405+combio-dku@users.noreply.github.com>
2023-09-10 15:52:47 +0900

    Add files via upload

---
+++ src/scodaviz/__init__.py
@@ -0,0 +1,4 @@
+# __init__.py
+# Copyright (c) 2021 (syoon@dku.edu) and contributors
+# https://github.com/combio-dku/MarkerCount/tree/master
+print('https://github.com/combio-dku')
---
+++ src/scodaviz/default_optional_files/analysis_config.py
@@ -0,0 +1,3 @@
+DEG_PVAL_CUTOFF_FOR_GSEA = 0.05
+MIN_NUM_CELLS_FOR_CCI_N_DEG = 40  # minimum number of cells for cell-cell interaction and DEG analysis
+REFERENCE_CELLTYPES_FOR_TUMOR_ID = ['T cell', 'B cell', 'Myeloid cell', 'Fibroblast', 'Endothelial cell', 'Smooth muscle cell', 'Granulocyte', 'Mast cell']
